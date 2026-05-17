ba4cf8feb56a12e700e822ef973c232ad0d7cfdd
PalNikolai4 <paha-87@mail.ru>
2026-05-17 14:11:00 +1000

    Setting up service files.
    - In 'vite.config.js' added setting 'css: {devSourcemap: true}'.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: 'source',
+    css: {
+        devSourcemap: true
+    },
     build: {
         outDir: '../dist',
         emptyOutDir: true
