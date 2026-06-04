e1d741ea3404a6df5b3bb61938ece0eb5c740e85
PalNikolai4 <paha-87@mail.ru>
2026-06-04 17:54:05 +1000

    chore(stylelint):add rule
    Added a rule to stylelint.config.js to check class naming.

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,12 @@ export default {
   extends: ['stylelint-config-standard-scss'],
 
   rules: {
-    'color-hex-length': 'long'
+    "color-hex-length": "long",
+    "selector-class-pattern": [
+      "^[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)?(--([a-z0-9]+-?)+)?$",
+      {
+        "message": "Expected class selector to follow BEM convention"
+      }
+    ]
   }
-
 }
