d73b0b3b22132d2949f667e01e7aa4999f12f200
PalNikolai4 <paha-87@mail.ru>
2026-07-01 15:55:47 +1000

    chore(main-nav):added script open-closed main-nav menu

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,17 @@
 import '../sass/style.scss';
+
+const mainNav = document.querySelector(".main-nav");
+const mainNavToggle = mainNav.querySelector(".main-nav__toggle");
+
+mainNav.classList.remove("main-nav--nojs");
+
+mainNavToggle.addEventListener('click', () => {
+  const isClosed = mainNav.classList.contains("main-nav--closed");
+  if (isClosed) {
+    mainNav.classList.remove("main-nav--closed");
+    mainNav.classList.add("main-nav--opened");
+  } else {
+    mainNav.classList.remove("main-nav--opened");
+    mainNav.classList.add("main-nav--closed");
+  }
+});
